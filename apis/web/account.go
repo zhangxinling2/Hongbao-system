dee4194057ae065bf5e34c4722280456f609dc8d
zhangxinling2 <2350486903@qq.com>
2023-09-28 16:12:09 +0800

    完成web api

--- apis/web/account.go
+++ apis/web/account.go
@@ -7,6 +7,10 @@ import (
 	service "resk/services"
 )
 
+const (
+	ResCodeBizTransferedFailure = base.ResCode(6010)
+)
+
 func init() {
 	infra.RegisterApi(new(AccountApi))
 }
@@ -16,30 +20,91 @@ type AccountApi struct {
 
 func (a *AccountApi) Init() {
 	groupRouter := base.Iris().Party("/v1/account")
-	create(groupRouter)
+	groupRouter.Post("/create", createHandler)
+	groupRouter.Post("/transfer", transferHandler)
+	groupRouter.Get("/get", getAccountHandler)
+	groupRouter.Get("/envelop/get", getEnvelopeAccountHandler)
 }
-func create(groupRouter iris.Party) {
-	groupRouter.Post("/create", func(context iris.Context) {
-		account := service.AccountCreatedDTO{}
-		err := context.ReadJSON(&account)
-		r := base.Res{
-			Code:    base.ResCodeOk,
-			Message: "",
-			Date:    nil,
-		}
-		if err != nil {
-			r.Code = base.ResCodeRequestParamsError
-			r.Message = err.Error()
-			context.JSON(r)
-			return
-		}
-		ser := service.GetAccountService()
-		dto, err := ser.CreateAccount(account)
-		if err != nil {
-			r.Code = base.ResCodeRequestParamsError
-			r.Message = err.Error()
-		}
-		r.Date = dto
-		context.JSON(&r)
-	})
+func createHandler(context iris.Context) {
+	account := service.AccountCreatedDTO{}
+	err := context.ReadJSON(&account)
+	r := base.Res{
+		Code:    base.ResCodeOk,
+		Message: "",
+		Date:    nil,
+	}
+	if err != nil {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = err.Error()
+		context.JSON(r)
+		return
+	}
+	ser := service.GetAccountService()
+	dto, err := ser.CreateAccount(account)
+	if err != nil {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = err.Error()
+	}
+	r.Date = dto
+	context.JSON(&r)
+}
+
+func transferHandler(context iris.Context) {
+	transfer := service.AccountTransferDTO{}
+	err := context.ReadJSON(&transfer)
+	r := base.Res{
+		Code:    base.ResCodeOk,
+		Message: "",
+		Date:    nil,
+	}
+	if err != nil {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = err.Error()
+		context.JSON(r)
+		return
+	}
+	ser := service.GetAccountService()
+	status, err := ser.Transfer(transfer)
+	if err != nil {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = err.Error()
+	}
+	r.Date = status
+	if status != service.TransferSuccess {
+		r.Code = ResCodeBizTransferedFailure
+		r.Message = err.Error()
+	}
+	context.JSON(&r)
+}
+func getEnvelopeAccountHandler(ctx iris.Context) {
+	userId := ctx.URLParam("userId")
+	r := base.Res{
+		Code: base.ResCodeOk,
+	}
+	if userId == "" {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = "ID不能为空"
+		ctx.JSON(&r)
+		return
+	}
+	ser := service.GetAccountService()
+	account := ser.GetEnvelopeAccountByUserId(userId)
+	r.Date = account
+	ctx.JSON(&r)
+}
+func getAccountHandler(ctx iris.Context) {
+	userId := ctx.URLParam("accountNo")
+	r := base.Res{
+		Code: base.ResCodeOk,
+	}
+	if userId == "" {
+		r.Code = base.ResCodeRequestParamsError
+		r.Message = "账户编号不能为空"
+		ctx.JSON(&r)
+		return
+	}
+	ser := service.GetAccountService()
+	account := ser.GetAccount(userId)
+	r.Date = account
+	ctx.JSON(&r)
 }
